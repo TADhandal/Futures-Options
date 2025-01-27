71e82fccdb4ca1c2eaa535a463056de6e74b9417
TADhandal <151362469+TADhandal@users.noreply.github.com>
2025-01-27 13:20:07 +0530

    Update Pay_Off_Diagram_(multiple_options)NEW.py

--- Pay_Off_Diagram_(multiple_options)NEW.py
+++ Pay_Off_Diagram_(multiple_options)NEW.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import opstrat as op
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+#st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.set_page_config(
     page_title="Pay Off Diagram for Options",
